dfe17c5f8f932a31f0f62951f866bc94d0b0f6ef
ivichadriana <108026185+ivichadriana@users.noreply.github.com>
2025-10-23 11:23:35 -0600

    added params to params.py of samples without stage info

--- src/params.py
+++ src/params.py
@@ -1,18 +1,15 @@
 # params.py
 
 fractions = ["Adipocytes", "Immune", "Stromal", "Epithelial"]
-
 subtype_order = ["Immunoreactive", "Differentiated", "Mesenchymal", "Proliferative"]
-
 subtype_map = {
     "IMR_consensus": "Immunoreactive",
     "DIF_consensus": "Differentiated",
     "MES_consensus": "Mesenchymal",
     "PRO_consensus": "Proliferative",
 }
-
 cont_cols = ["Age", "BMI"]
-
 custom_colors = ["green", "#FFD700", "darkorange", "red"]
 colors = ["peachpuff", "orange", "tomato", "salmon"]
-levels = ["Immunoreactive", "Differentiated", "Mesenchymal", "Proliferative"]
+levels = subtype_order
+no_adj_samples= [130131, 130138, 161063, 161090, 170110, 190001, 190010]
